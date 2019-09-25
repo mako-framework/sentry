c23ecd4cbfe118dc9f6a0c6e1f82b38b9ca19217
Frederic G. Østby <frederic.g.ostby@gmail.com>
2019-09-25 16:41:45 +0200

    Bind the created client

--- src/services/LoggerService.php
+++ src/services/LoggerService.php
@@ -12,6 +12,7 @@ use mako\sentry\HandlerDecorator;
 use Monolog\Formatter\LineFormatter;
 use Sentry\ClientBuilder;
 use Sentry\Monolog\Handler;
+use Sentry\SentrySdk;
 use Sentry\State\Hub;
 
 /**
@@ -27,6 +28,8 @@ class LoggerService extends Service
 	protected function getSentryHandler(): HandlerDecorator
 	{
 		$client = ClientBuilder::create(['dsn' => $this->config->get('application.sentry_dsn')])->getClient();
+
+		SentrySdk::init()->bindClient($client);
 
 		$handler = new HandlerDecorator(new Handler(new Hub($client)));
 
